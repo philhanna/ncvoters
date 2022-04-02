1ddf227ab67a4e3d7b71eacc0dae300bbd157665
Phil Hanna <ph1204@gmail.com>
2022-04-01 20:51:10 -0400

    Test for how to read CSV directly from zip

---
+++ tests/spike_unzip_blocks.py
@@ -0,0 +1,25 @@
+import csv
+import io
+import unittest
+import zipfile
+
+from voters import ZIP_FILE_NAME
+
+
+class MyTestCase(unittest.TestCase):
+    def test_blocked(self):
+        expected = 8538323
+        with zipfile.ZipFile(ZIP_FILE_NAME) as zipf:
+            with zipf.open("ncvoter_Statewide.txt", "r") as f:
+                reader = csv.reader(io.TextIOWrapper(f, encoding="iso8859"), delimiter='\t')
+                count = 0
+                for row in reader:
+                    count += 1
+                    if count > 10:
+                        break
+                    print(f"DEBUG: row={row}")
+        #self.assertEqual(expected, count)
+
+
+if __name__ == '__main__':
+    unittest.main()
