e3c3ae6b408822019dd34464fd24ea6faa4c2495
Phil Hanna <ph1204@gmail.com>
2022-04-01 22:42:52 -0400

    Added docstring

--- voters/extractor.py
+++ voters/extractor.py
@@ -8,11 +8,19 @@ from voters import ZIP_FILE_NAME, TEXT_FILE_NAME, ENCODING, COLUMNS
 class Extractor:
     """ Extracts specific columns from CSV file """
 
+    @staticmethod
+    def select_columns(row, column_list=COLUMNS.keys()):
+        outrow = list()
+        for colnum in column_list:
+            outrow.append(row[colnum])
+        return outrow
+
     def __init__(self, filename=TEXT_FILE_NAME, zipfile=ZIP_FILE_NAME):
         self.zip_file_name = zipfile
         self.internal_file_name = filename
 
     def get_rows(self, limit=0):
+        """ A generator that returns rows filtered by column list """
         with zipfile.ZipFile(self.zip_file_name) as archive:
             with archive.open(self.internal_file_name, "r") as fp:
 
@@ -28,9 +36,3 @@ class Extractor:
                     outrow = Extractor.select_columns(row)
                     yield outrow
 
-    @staticmethod
-    def select_columns(row, column_list=COLUMNS.keys()):
-        outrow = list()
-        for colnum in column_list:
-            outrow.append(row[colnum])
-        return outrow
