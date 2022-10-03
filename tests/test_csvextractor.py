04a1cd4eb0cf745aabd0667f8ab35f3bb2af315e
Phil Hanna <ph1204@gmail.com>
2022-10-03 19:25:29 -0400

    Issue #11: Use testdata sample file for input

--- tests/test_csvextractor.py
+++ tests/test_csvextractor.py
@@ -1,9 +1,15 @@
+import os
 from unittest import TestCase
 
+from tests import testdata
 from voters import COLUMNS, CSVExtractor
 
 
 class TestExtractor(TestCase):
+
+    def setUp(self):
+        self.zipfile = os.path.join(testdata, "ten_voters.zip")
+        self.filename = "ten_voters.txt"
 
     def test_columns(self):
         self.assertEqual(20, len(COLUMNS))
@@ -17,8 +23,8 @@ class TestExtractor(TestCase):
         self.assertListEqual(['baker', 'charlie'], outrow)
 
     def test_get_rows_with_limit(self):
-        extractor = CSVExtractor()
-        expected = 10
+        extractor = CSVExtractor(filename=self.filename,zipfile=self.zipfile)
+        expected = 4
         actual = 0
         for row in extractor.get_rows(limit=expected):
             actual += 1
