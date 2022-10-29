d5dfd6b6cefcc4dd6519f0a917fe3da8f62c245d
Phil Hanna <ph1204@gmail.com>
2022-10-29 17:01:04 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ncvoters',
+    version='1.2.0',
+    packages=[''],
+    url='https://github.com/philhanna/ncvoters',
+    license='MIT license',
+    author='Phil Hanna',
+    author_email='ph1204@gmail.com',
+    description='Allows the user to download selected rows and columns from North Carolina voter registration data'
+)
