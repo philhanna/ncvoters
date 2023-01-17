a2175985a401a9e487162acdedc56bfc1575f270
Phil Hanna <ph1204@gmail.com>
2023-01-16 20:08:18 -0500

    Added testdata and unit tests

--- go/dv_test.go
+++ go/dv_test.go
@@ -1,17 +1,27 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestExistingFile(t *testing.T) {
 	filename := "go.mod"
 	if !fileExists(filename) {
-		t.Errorf("%s file exists but is not detected", filename)
+		t.Errorf("%s file exists but was not detected", filename)
 	}
 }
 
 func TestNonExistingFile(t *testing.T) {
 	filename := "bogus"
 	if fileExists(filename) {
-		t.Errorf("%s file does not exist but is detected", filename)
+		t.Errorf("%s file does not exist but was detected", filename)
+	}
+}
+
+func TestPartialZip(t *testing.T) {
+	zipFileName := "testdata/partial.zip"
+	_, err := assertZipfileIsntPartial(zipFileName)
+	if err == nil {
+		t.Errorf("%s should have been detected as partial", zipFileName)
 	}
 }
