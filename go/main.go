ff496236a1c5df89c5a7b18850261e2455c68e06
Phil Hanna <ph1204@gmail.com>
2023-01-18 00:19:22 -0500

    Refactored

--- go/main.go
+++ go/main.go
@@ -6,6 +6,11 @@ import (
 	"path/filepath"
 )
 
+type Column struct {
+	number int    // column number
+	name   string // column name
+}
+
 const (
 	// Where to find the zip file, and the name for the text file inside it
 	dataSourceUrl = "https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter_Statewide.zip"
@@ -20,60 +25,57 @@ const (
 	zipChunkSize = 16 * 1024 * 1024
 )
 
-// Path to the database to be created
-var dbFileName = filepath.Join(os.TempDir(), "ncvoters.db")
+var (
+	// Path to the database to be created
+	dbFileName = filepath.Join(os.TempDir(), "ncvoters.db")
 
-// selectedColumns is a map of column numbers to column names for the subset
-// of selectedColumns we want.
-//
-// You can adjust this by adding or deleting lines. The selectedColumns are
-// numbered starting with 0 for county_id to 66 for vtd_desc.
-//
-// See ncvoters.md for the list of all selectedColumns available.
-var selectedColumns = map[int]string{
-	0:  "county_id",
-	2:  "voter_reg_num",
-	4:  "last_name",
-	5:  "first_name",
-	6:  "middle_name",
-	7:  "name_suffix_lbl",
-	8:  "status_cd",
-	10: "reason_cd",
-	12: "res_street_address",
-	13: "res_city_desc",
-	14: "state_cd",
-	15: "zip_code",
-	23: "full_phone_number",
-	26: "race_code",
-	27: "ethnic_code",
-	28: "party_cd",
-	29: "gender_code",
-	30: "birth_year",
-	31: "age_at_year_end",
-	32: "birth_state",
-}
+	// selectedColumns is a map of column numbers to column names for the subset
+	// of selectedColumns we want.
+	//
+	// You can adjust this by adding or deleting lines. The selectedColumns are
+	// numbered starting with 0 for county_id to 66 for vtd_desc.
+	//
+	// See ncvoters.md for the list of all selectedColumns available.
+	selectedColumns = map[int]string{
+		0:  "county_id",
+		2:  "voter_reg_num",
+		4:  "last_name",
+		5:  "first_name",
+		6:  "middle_name",
+		7:  "name_suffix_lbl",
+		8:  "status_cd",
+		10: "reason_cd",
+		12: "res_street_address",
+		13: "res_city_desc",
+		14: "state_cd",
+		15: "zip_code",
+		23: "full_phone_number",
+		26: "race_code",
+		27: "ethnic_code",
+		28: "party_cd",
+		29: "gender_code",
+		30: "birth_year",
+		31: "age_at_year_end",
+		32: "birth_state",
+	}
 
-type Column struct {
-	number int    // column number
-	name   string // column name
-}
-
-var columns = getColumnsAsStruct()
+	columns = getColumnsAsStruct()
+)
 
 func main() {
+	var filesize int64
+	var err error
 
 	// 1. Download the latest zip file
 
-	filesize, err := step1()
-	if err != nil {
+	if filesize, err = step1(); err != nil {
 		log.Fatalf("Step 1 failed: %v", err)
 	}
 	log.Printf("zip file size is %d bytes", filesize)
 
-	err = step2()
-	if err != nil {
+	// 2. Create an empty voter database with just the table definition.
+	if err = step2(); err != nil {
 		log.Fatalf("Step 2 failed: %v", err)
 	}
 	log.Println("Created empty database", dbFileName)
-
 }
